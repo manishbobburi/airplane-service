cf5682110823de8c2d54462f53c7eff2f71d1f75
MANISH6148 <manishbobburi6148@gmail.com>
2025-06-22 12:38:29 +0530

    feat(repository): implement CrudRepository class with CRUD methods
    Added a CrudRepository class with the following methods: create, destroy, update, get, and getAll.
    This serves as a basic template for performing CRUD operations with any model.

---
+++ src/repositories/crud-repository.js
@@ -0,0 +1,66 @@
+const { Logger } = require("../config/index");
+
+class CrudRepository {
+    constructor(model) {
+        this.model = model;
+    }
+
+    async create(data) {
+        try {
+            const response = await this.model.create(data);
+            return response;
+        } catch (error) {
+            Logger.error("Some thing went wrong in the Crud Repo: create");
+            throw error;
+        }
+    }
+
+    async destroy(data) {
+        try {
+            const response = await this.model.destroy({
+                where: {
+                   id: data,
+                },
+            });
+            return response;
+        } catch (error) {
+            Logger.error("Some thing went wrong in the Crud Repo: destroy");
+            throw error;
+        }
+    }
+
+    async get(data) {
+        try {
+            const response = await this.model.findByPk(data);
+            return response;
+        } catch (error) {
+            Logger.error("Some thing went wrong in the Crud Repo: get");
+            throw error;
+        }
+    }
+
+    async getAll() {
+        try {
+            const response = await this.model.findAll();
+            return response;
+        } catch (error) {
+            Logger.error("Some thing went wrong in the Crud Repo: getAll");
+            throw error;
+        }
+    }
+
+    async update(id, data) { // data -> {key: value, ...}
+        try {
+            const response = await this.model.update(data, {
+                id: id,
+            });
+            return response;
+        } catch (error) {
+            Logger.error("Some thing went wrong in the Crud Repo: update");
+            throw error;
+        }
+    }
+
+}
+
+module.exports = CrudRepository;
