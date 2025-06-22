0d0ca37138ad86669df3ae1b8f7dddc7c974fe41
MANISH6148 <manishbobburi6148@gmail.com>
2025-06-22 19:10:52 +0530

    add index.js files

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { InfoController } = require("../../controllers");
 const router = express.Router();
+const airplaneRoutes = require("./airplane-routes");
 
-router.get("/info", InfoController.info);
+router.use("/airplanes", airplaneRoutes);
 
 module.exports = router;
