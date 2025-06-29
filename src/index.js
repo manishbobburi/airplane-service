c9199151bb8d3adb25dd7e429e32a16bb666c819
MANISH6148 <manishbobburi6148@gmail.com>
2025-06-30 00:07:32 +0530

    feat(model): create flights model with foreign key constraint defined during model defination

--- src/index.js
+++ src/index.js
@@ -10,14 +10,5 @@ app.use(express.urlencoded({extended: true}));
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
-    const { Airport, City } = require("./models");
     console.log(`Server started running on PORT: ${ServerConfig.PORT}`);
-    // const hyderabad = await City.findByPk(11);
-    // const rjiAirport = await Airport.findByPk(23)
-    // Airport.destroy({where:{id: 13}});
-    // hyderabad.createAirport({name: "Manish Intl", code: "BND", address: null});
-    await City.destroy({where:{
-        id:4,
-    }});
-    // hyderabad.removeAirport(rjiAirport);
 });
