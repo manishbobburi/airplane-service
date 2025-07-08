bf16f509af7a0a2d463c0f2a502e89e32766cadc
MANISH6148 <manishbobburi6148@gmail.com>
2025-07-08 22:38:13 +0530

    feat(repositories): add un-managed transaction for method updateRemainingSeats

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -43,14 +43,16 @@ class FlightRepository extends CrudRepository {
         return flights;
     }
 
-    async updateRemainingSeats(flightId, seats, dec = 1) {
+    async updateRemainingSeats(flightId, seats, dec = true) {
+        const transaction = await db.sequelize.transaction();
         await db.sequelize.query(addRowLock(flightId));
         const flight = await Flight.findByPk(flightId);
-        if(parseInt(dec)) {
-            await flight.decrement('totalSeats', {by : seats});
+        if(+dec) {
+            await flight.decrement('totalSeats', {by : seats}, {transaction: transaction});
         } else {
-            await flight.increment('totalSeats', {by: seats});
+            await flight.increment('totalSeats', {by: seats}, {transaction: transaction});
         }
+        await transaction.commit();
         await flight.reload();
         return flight;
     }
